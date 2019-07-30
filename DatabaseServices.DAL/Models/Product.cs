f81b56f382f474b0aa226d646c0f736dcfc73cfa
Горелкин Александр <s207883@yandex.ru>
2019-07-30 12:11:57 +0300

    Добавлена базовая модель продукта

---
+++ DatabaseServices.DAL/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseServices.DAL.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public int SellerId { get; set; }
+		public string ProductName { get; set; }
+		public string Description { get; set; }
+		public string MeasureUnit { get; set; }
+		public string Category { get; set; }
+		public int Amount { get; set; }
+		public decimal Price { get; set; }
+		public string QrCode { get; set; }
+	}
+}
