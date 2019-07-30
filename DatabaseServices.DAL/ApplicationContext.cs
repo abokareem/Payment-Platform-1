6a706a1208d3136d32d7a0ddb8443e2b1e6b1540
Горелкин Александр <s207883@yandex.ru>
2019-07-30 13:47:57 +0300

    Рефакторинг контекста

--- DatabaseServices.DAL/ApplicationContext.cs
+++ DatabaseServices.DAL/ApplicationContext.cs
@@ -24,44 +24,104 @@ namespace DatabaseServices.DAL
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			#region Transaction
-			modelBuilder.Entity<Transaction>().Property(p => p.TransactionTime).IsRequired().HasDefaultValueSql("GETDATE()");
-			modelBuilder.Entity<Transaction>().Property(p => p.UniqueHashNumber).IsRequired();
+			#region Transaction table
+			modelBuilder.Entity<Transaction>()
+				.Property(p => p.TransactionTime)
+				.IsRequired()
+				.HasDefaultValueSql("GETDATE()");
+			modelBuilder.Entity<Transaction>()
+				.Property(p => p.UniqueHashNumber)
+				.IsRequired();
+			//Foreign Keys
+			modelBuilder.Entity<Transaction>()
+				.HasOne(t => t.Seller)
+				.WithMany(s => s.Transactions)
+				.HasForeignKey(p => p.SellerId)
+				.OnDelete(DeleteBehavior.Restrict);
 
-			modelBuilder.Entity<Transaction>().HasOne(t => t.Seller).WithMany(s => s.Transactions).HasForeignKey(p => p.SellerId);
-			modelBuilder.Entity<Transaction>().HasOne(t => t.Buyer).WithMany(s => s.Transactions).HasForeignKey(p => p.BuyerId);
-			modelBuilder.Entity<Transaction>().HasOne(t => t.Product).WithMany(s => s.Transactions).HasForeignKey(p => p.ProductId);
-			#endregion
-			#region Customer
-			modelBuilder.Entity<Customer>().Property(p => p.FirstName).IsRequired();
-			modelBuilder.Entity<Customer>().Property(p => p.MiddleName).IsRequired();
-			modelBuilder.Entity<Customer>().Property(p => p.LastName).IsRequired();
-			modelBuilder.Entity<Customer>().Property(p => p.Email).IsRequired();
+			modelBuilder.Entity<Transaction>()
+				.HasOne(t => t.Buyer)
+				.WithMany(s => s.Transactions)
+				.HasForeignKey(p => p.BuyerId)
+				.OnDelete(DeleteBehavior.Restrict);
 
-
-			modelBuilder.Entity<Customer>().HasOne(t => t.Seller).WithMany(s => s.Customers).HasForeignKey(p => p.SellerId);
-			modelBuilder.Entity<Customer>().HasOne(t => t.Buyer).WithMany(s => s.Customers).HasForeignKey(p => p.BuyerId);
-			#endregion
-
-			#region Product
-			modelBuilder.Entity<Product>().Property(p => p.ProductName).IsRequired();
-			modelBuilder.Entity<Product>().Property(p => p.Description).IsRequired();
-			modelBuilder.Entity<Product>().Property(p => p.MeasureUnit).IsRequired();
-			modelBuilder.Entity<Product>().Property(p => p.Category).IsRequired();
-			modelBuilder.Entity<Product>().Property(p => p.QrCode).IsRequired();
-
-			modelBuilder.Entity<Product>().HasOne(t => t.Seller).WithMany(s => s.Products).HasForeignKey(p => p.SellerId);
-			#endregion
-
-			#region Buyer
-			modelBuilder.Entity<Buyer>().Property(p => p.Billing).IsRequired();
+			modelBuilder.Entity<Transaction>()
+				.HasOne(t => t.Product)
+				.WithMany(s => s.Transactions)
+				.HasForeignKey(p => p.ProductId)
+				.OnDelete(DeleteBehavior.Restrict);
 			#endregion
 
-			#region Seller
-			modelBuilder.Entity<Seller>().Property(p => p.OrganisationName).IsRequired();
-			modelBuilder.Entity<Seller>().Property(p => p.OrganisationNumber).IsRequired();
-			modelBuilder.Entity<Seller>().Property(p => p.ResponsiblePerson).IsRequired();
-			modelBuilder.Entity<Seller>().Property(p => p.Billing).IsRequired();
+			#region Customer table
+			modelBuilder.Entity<Customer>()
+				.Property(p => p.FirstName)
+				.IsRequired();
+			modelBuilder.Entity<Customer>()
+				.Property(p => p.MiddleName)
+				.IsRequired();
+			modelBuilder.Entity<Customer>()
+				.Property(p => p.LastName)
+				.IsRequired();
+			modelBuilder.Entity<Customer>()
+				.Property(p => p.Email)
+				.IsRequired();
+			//Foreign Keys
+			modelBuilder.Entity<Customer>()
+				.HasOne(t => t.Seller)
+				.WithMany(s => s.Customers)
+				.HasForeignKey(p => p.SellerId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			modelBuilder.Entity<Customer>()
+				.HasOne(t => t.Buyer)
+				.WithMany(s => s.Customers)
+				.HasForeignKey(p => p.BuyerId)
+				.OnDelete(DeleteBehavior.Restrict);
+			#endregion
+
+			#region Product table
+			modelBuilder.Entity<Product>()
+				.Property(p => p.ProductName)
+				.IsRequired();
+			modelBuilder.Entity<Product>()
+				.Property(p => p.Description)
+				.IsRequired();
+			modelBuilder.Entity<Product>()
+				.Property(p => p.MeasureUnit)
+				.IsRequired();
+			modelBuilder.Entity<Product>()
+				.Property(p => p.Category)
+				.IsRequired();
+			modelBuilder.Entity<Product>()
+				.Property(p => p.QrCode)
+				.IsRequired();
+			//Foreign Keys
+			modelBuilder.Entity<Product>()
+				.HasOne(t => t.Seller)
+				.WithMany(s => s.Products)
+				.HasForeignKey(p => p.SellerId)
+				.OnDelete(DeleteBehavior.Restrict);
+			#endregion
+
+			#region Buyer table
+			modelBuilder.Entity<Buyer>()
+				.Property(p => p.Billing)
+				.IsRequired();
+			#endregion
+
+			#region Seller table
+			modelBuilder.Entity<Seller>()
+				.Property(p => p.OrganisationName)
+				.IsRequired();
+			modelBuilder.Entity<Seller>()
+				.Property(p => p.OrganisationNumber)
+				.IsRequired();
+			modelBuilder.Entity<Seller>()
+				.Property(p => p.ResponsiblePerson)
+				.IsRequired();
+			modelBuilder.Entity<Seller>()
+				.Property(p => p.Billing)
+				.IsRequired();
 			#endregion
 		}
 	}
