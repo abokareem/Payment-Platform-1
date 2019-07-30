f41b43a7290623e92e58988b8bfeca9f0c56c3ff
Горелкин Александр <s207883@yandex.ru>
2019-07-30 16:18:05 +0300

    Реализован консольный интерфейс

--- DatabaseServices/Program.cs
+++ DatabaseServices/Program.cs
@@ -9,18 +9,52 @@ namespace DatabaseServices.View
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Сервис для генерирования БД и тестовых данных для нее");
+
+			Selector();
+		}
+
+		static void Selector()
+		{
+			bool exit = false;
+
 			var context = new ApplicationContext();
 			IRandomDataGenerator randomDataGenerator = new RandomDataGenerator(context);
-
 			var dbController = new DatabaseController(context);
-			var dbDeletionResult = dbController.DeleteDatabaseAsync().Result;
-			var dbCreationResult = dbController.CreateDatabaseAsync().Result;
-			var randomAdditisionResult = dbController.AddRandomDataToDatabaseAsync(randomDataGenerator).Result;
-			Console.WriteLine(dbDeletionResult);
-			Console.WriteLine(dbCreationResult);
-			Console.WriteLine(randomAdditisionResult);
-			Console.ReadLine();
+
+			Console.WriteLine("Управление:");
+			Console.WriteLine("Нажмите С - для создания БД");
+			Console.WriteLine("Нажмите D - для удаления БД");
+			Console.WriteLine("Нажмите R - для добавления случайных записей в БД (при множественном использовании адекватную работу не гарантирую)");
+			Console.WriteLine("Нажмите E - для выхода");
+
+			while (!exit)
+			{
+				var key = Console.ReadKey().Key;
+				Console.WriteLine();
+				string operationResult;
+				switch (key)
+				{
+					case ConsoleKey.C:
+						operationResult = dbController.CreateDatabaseAsync().Result == true ? "успешно" : "с ошибкой";
+						Console.WriteLine($"Работа завершена {operationResult}");
+						break;
+					case ConsoleKey.D:
+						operationResult = dbController.DeleteDatabaseAsync().Result == true ? "успешно" : "с ошибкой";
+						Console.WriteLine($"Работа завершена {operationResult}");
+						break;
+					case ConsoleKey.R:
+						operationResult = dbController.AddRandomDataToDatabaseAsync(randomDataGenerator).Result == true ? "успешно" : "с ошибкой";
+						Console.WriteLine($"Работа завершена {operationResult}");
+						break;
+					case ConsoleKey.E:
+						Console.WriteLine("Завершение работы");
+						exit = true;
+						break;
+					default:
+						break;
+				}
+			}
 		}
 	}
 }
