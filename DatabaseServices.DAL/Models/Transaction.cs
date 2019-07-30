ecd5e3e16adfba7f6a1fd8fb35e256622bcde35f
Горелкин Александр <s207883@yandex.ru>
2019-07-30 12:16:11 +0300

    Реализована базовая модель транзакции

---
+++ DatabaseServices.DAL/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseServices.DAL.Models
+{
+	public class Transaction
+	{
+		public int Id { get; set; }
+		public string UniqueHashNumber { get; set; }
+		public int SellerId { get; set; }
+		public int BuyerId { get; set; }
+		public int ProductId { get; set; }
+		public DateTime TransactionTime { get; set; }
+		public int TransactionStatus { get; set; }
+	}
+}
