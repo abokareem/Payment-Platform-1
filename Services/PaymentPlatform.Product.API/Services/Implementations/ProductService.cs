4266b7951e6680ecf1b8dbdbf6df420c4e1cb39c
Горелкин Александр <s207883@yandex.ru>
2019-08-05 16:24:29 +0300

    Добавление DI.

--- Services/PaymentPlatform.Product.API/Services/Implementations/ProductService.cs
+++ Services/PaymentPlatform.Product.API/Services/Implementations/ProductService.cs
@@ -1,9 +1,9 @@
-﻿using PaymentPlatform.Product.API.Models;
+﻿using AutoMapper;
+using PaymentPlatform.Product.API.Models;
 using PaymentPlatform.Product.API.Services.Interfaces;
 using PaymentPlatform.Product.API.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PaymentPlatform.Product.API.Services.Implementations
@@ -11,6 +11,13 @@ namespace PaymentPlatform.Product.API.Services.Implementations
 	public class ProductService : IProductService
 	{
 		private readonly ProductContext _productContext;
+		private readonly IMapper _mapper;
+
+		public ProductService(ProductContext productContext, IMapper mapper)
+		{
+			_productContext = productContext;
+			_mapper = mapper;
+		}
 
 		public Task<int> AddNewProductAsync(ProductViewModel productViewModel, UserViewModel userViewModel)
 		{
--- Services/PaymentPlatform.Product.API/Startup.cs
+++ Services/PaymentPlatform.Product.API/Startup.cs
@@ -1,34 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PaymentPlatform.Product.API.Helpers;
+using PaymentPlatform.Product.API.Models;
+using PaymentPlatform.Product.API.Services.Implementations;
+using PaymentPlatform.Product.API.Services.Interfaces;
 
 namespace PaymentPlatform.Product.API
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			string connectionString = Configuration.GetConnectionString("DefaultConnection");
+			services.AddDbContext<ProductContext>(options => options.UseSqlServer(connectionString));
+
+			var mappingConfig = new MapperConfiguration(mc =>
+			{
+				mc.AddProfile(new MappingProfile());
+			});
+
+			IMapper mapper = mappingConfig.CreateMapper();
+			services.AddSingleton(mapper);
+
+			services.AddScoped<IProductService, ProductService>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Hello World!");
+			});
+		}
+	}
 }
