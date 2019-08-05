711e585e8229c4843ac2b994135c5c7358312716
Горелкин Александр <s207883@yandex.ru>
2019-08-05 16:12:51 +0300

    Через NuGet добавлен Automapper.

---
+++ Services/PaymentPlatform.Product.API/Helpers/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentPlatform.Product.API.Helpers
+{
+	public class MappingProfile:Profile
+	{
+	}
+}
--- Services/PaymentPlatform.Product.API/Services/Implementations/ProductService.cs
+++ Services/PaymentPlatform.Product.API/Services/Implementations/ProductService.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using PaymentPlatform.Product.API.Models;
+using PaymentPlatform.Product.API.Services.Interfaces;
+using PaymentPlatform.Product.API.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PaymentPlatform.Product.API.Services.Implementations
 {
-	public class ProductService
+	public class ProductService : IProductService
 	{
+		private readonly ProductContext _productContext;
+
+		public Task<int> AddNewProductAsync(ProductViewModel productViewModel, UserViewModel userViewModel)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<ProductViewModel>> GetAllProductsAsyc(int? take = null, int? skip = null)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ProductViewModel> GetProductByIdAsync(Guid productId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<ProductViewModel>> GetProductsByUserIdAsyc(UserViewModel userViewModel, int? take = null, int? skip = null)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateProductAsync(ProductViewModel productViewModel)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
