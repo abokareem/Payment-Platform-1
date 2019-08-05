53f4c484c9f74224b65a730371b4af4c1f13d226
Горелкин Александр <s207883@yandex.ru>
2019-08-05 16:09:11 +0300

    Добавлена модель товара. Добавлен контекст работы с БД. Добавлен Интерфейс менедженра товаров. Добавлены модели представления пользователя и товара.

---
+++ Services/PaymentPlatform.Product.API/Services/Implementations/ProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentPlatform.Product.API.Services.Implementations
+{
+	public class ProductService
+	{
+	}
+}
---
+++ Services/PaymentPlatform.Product.API/ViewModels/ProductViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentPlatform.Product.API.ViewModels
+{
+	public class ProductViewModel
+	{
+		/// <summary>
+		/// Название товара.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Описание товара.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Единица измерения.
+		/// </summary>
+		public string MeasureUnit { get; set; }
+
+		/// <summary>
+		/// Категория.
+		/// </summary>
+		public string Category { get; set; }
+
+		/// <summary>
+		/// Количество.
+		/// </summary>
+		public int Amount { get; set; }
+
+		/// <summary>
+		/// Цена.
+		/// </summary>
+		public decimal Price { get; set; }
+	}
+}
