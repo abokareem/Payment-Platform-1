2e78ed1e5489779ed53e5034e9ee32970ede9e89
Михаил Масный <masny.mikhail@gmail.com>
2019-08-09 22:10:43 +0300

    Реализован сервис.

--- PaymentPlatform.Profile.API/Services/Implementations/ProfileService.cs
+++ PaymentPlatform.Profile.API/Services/Implementations/ProfileService.cs
@@ -1,7 +1,11 @@
-﻿using PaymentPlatform.Profile.API.Services.Interfaces;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PaymentPlatform.Profile.API.Models;
+using PaymentPlatform.Profile.API.Services.Interfaces;
 using PaymentPlatform.Profile.API.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PaymentPlatform.Profile.API.Services.Implementations
@@ -11,28 +15,89 @@ namespace PaymentPlatform.Profile.API.Services.Implementations
     /// </summary>
     public class ProfileService : IProfileService
     {
-        /// <inheritdoc/>
-        public Task<string> AddNewProfileAsync(ProfileViewModel profileViewModel, UserViewModel userViewModel)
+        private readonly ProfileContext _profileContext;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="profileContext">контекст.</param>
+        /// <param name="mapper">профиль AutoMapper.</param>
+        public ProfileService(ProfileContext profileContext, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _profileContext = profileContext;
+            _mapper = mapper;
         }
 
         /// <inheritdoc/>
-        public Task<List<ProfileViewModel>> GetAllProfilesAsyc(int? take = null, int? skip = null)
+        public async Task<string> AddNewProfileAsync(ProfileViewModel profileViewModel, UserViewModel userViewModel)
         {
-            throw new NotImplementedException();
+            var profile = _mapper.Map<Models.Profile>(profileViewModel);
+
+            await _profileContext.Profiles.AddAsync(profile);
+            await _profileContext.SaveChangesAsync();
+
+            var id = profile.Id.ToString();
+
+            return id;
         }
 
         /// <inheritdoc/>
-        public Task<ProfileViewModel> GetProfileByIdAsync(Guid profileId)
+        public async Task<List<ProfileViewModel>> GetAllProfilesAsyc(int? take = null, int? skip = null)
         {
-            throw new NotImplementedException();
+            var queriableListOfProfiles = _profileContext.Profiles.Select(x => x);
+
+            if (take != null && take > 0 && skip != null && skip > 0)
+            {
+                queriableListOfProfiles = queriableListOfProfiles.Skip((int)skip).Take((int)take);
+            }
+
+            var listOfProfiles = await queriableListOfProfiles.ToListAsync();
+
+
+            var listOfProfilesViewModels = new List<ProfileViewModel>();
+
+            foreach (var profileModel in listOfProfiles)
+            {
+                var profileViewModel = _mapper.Map<ProfileViewModel>(profileModel);
+                listOfProfilesViewModels.Add(profileViewModel);
+            }
+
+            return listOfProfilesViewModels;
         }
 
         /// <inheritdoc/>
-        public Task<bool> UpdateProfileAsync(ProfileViewModel profileViewModel)
+        public async Task<ProfileViewModel> GetProfileByIdAsync(Guid profileId)
         {
-            throw new NotImplementedException();
+            var profile = await _profileContext.Profiles.FirstOrDefaultAsync(p => p.Id == profileId);
+            var profileViewModel = _mapper.Map<ProfileViewModel>(profile);
+
+            return profileViewModel;
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> UpdateProfileAsync(ProfileViewModel profileViewModel)
+        {
+            var profile = await _profileContext.Profiles.FirstOrDefaultAsync(p => p.Id == profileViewModel.Id);
+
+            if (profile == null)
+            {
+                return false;
+            }
+
+            profile.FirstName = profileViewModel.FirstName;
+            profile.LastName = profileViewModel.LastName;
+            profile.SecondName = profileViewModel.SecondName;
+            profile.IsSeller = profileViewModel.IsSeller;
+            profile.OrgName = profileViewModel.OrgName;
+            profile.OrgNumber = profileViewModel.OrgNumber;
+            profile.BankBook = profileViewModel.BankBook;
+            profile.Balance = profileViewModel.Balance;
+
+            _profileContext.Update(profile);
+            await _profileContext.SaveChangesAsync();
+
+            return true;
         }
     }
 }
