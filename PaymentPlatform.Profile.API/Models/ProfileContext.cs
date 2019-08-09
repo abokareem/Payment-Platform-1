06bf0dd492f3b6d1bf772c0a2151c1380fc25f32
Михаил Масный <masny.mikhail@gmail.com>
2019-08-09 20:36:11 +0300

    Добавлен контекст базы данных для EF Core.

---
+++ PaymentPlatform.Profile.API/Models/ProfileContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentPlatform.Profile.API.Models
+{
+    public class ProfileContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+
+        public ProfileContext(DbContextOptions<ProfileContext> options) : base(options) { }
+    }
+}
