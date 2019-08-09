71d4fc047a1a4499e0cfc3a811f908e73c99da80
Михаил Масный <masny.mikhail@gmail.com>
2019-08-09 21:15:15 +0300

    Добавлен интерфейс и ViewModel.

---
+++ PaymentPlatform.Profile.API/Services/Interfaces/IProfileService.cs
@@ -0,0 +1,40 @@
+﻿using PaymentPlatform.Profile.API.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymentPlatform.Profile.API.Services.Interfaces
+{
+    interface IProfileService
+    {
+        /// <summary>
+		/// Добавить новый профиль.
+		/// </summary>
+		/// <param name="profileViewModel">Модель профиля.</param>
+		/// <param name="userViewModel">Пользователь.</param>
+		/// <returns>Id профиля.</returns>
+		Task<string> AddNewProfileAsync(ProfileViewModel profileViewModel, UserViewModel userViewModel);
+
+        /// <summary>
+        /// Получить профиль по его Id.
+        /// </summary>
+        /// <param name="profileId">Id профиля.</param>
+        /// <returns>ViewModel профиля.</returns>
+        Task<ProfileViewModel> GetProfileByIdAsync(Guid profileId);
+
+        /// <summary>
+        /// Получить все профили.
+        /// </summary>
+        /// <param name="take">параметр пагинации (кол-во взять).</param>
+        /// <param name="skip">параметр пагинации (кол-во пропустить).</param>
+        /// <returns>Список профилей.</returns>
+        Task<List<ProfileViewModel>> GetAllProfilesAsyc(int? take = null, int? skip = null);
+
+        /// <summary>
+        /// Обновить свойства профиля.
+        /// </summary>
+        /// <param name="profileViewModel">Профиль.</param>
+        /// <returns>Результат операции.</returns>
+        Task<bool> UpdateProfileAsync(ProfileViewModel profileViewModel);
+    }
+}
---
+++ PaymentPlatform.Profile.API/ViewModels/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentPlatform.Profile.API.ViewModels
+{
+    /// <summary>
+    /// ViewModel для пользователя.
+    /// </summary>
+	public class UserViewModel
+    {
+        /// <summary>
+        /// Идентификатор (GUID).
+        /// </summary>
+		public Guid Id { get; set; }
+
+        /// <summary>
+        /// Роль.
+        /// </summary>
+		public string Role { get; set; }
+    }
+}
