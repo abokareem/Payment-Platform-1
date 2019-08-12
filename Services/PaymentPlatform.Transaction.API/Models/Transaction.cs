84ece7af230c01dba90a767e25a30ffc9b4e7b2d
Горелкин Александр <s207883@yandex.ru>
2019-08-12 17:48:40 +0300

    Создан контекст API. Добавлены модели.

---
+++ Services/PaymentPlatform.Transaction.API/Models/Transaction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentPlatform.Transaction.API.Models
+{
+	/// <summary>
+	/// Модель транзакции.
+	/// </summary>
+	public class Transaction
+	{
+		/// <summary>
+		/// Идентификатор (GUID).
+		/// </summary>
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Идентификатор покупателя.
+		/// </summary>
+		public Guid ProfileId { get; set; }
+
+		/// <summary>
+		/// Идентификатор продукта.
+		/// </summary>
+		public Guid ProductId { get; set; }
+
+        /// <summary>
+        /// Время совершения операции.
+        /// </summary>
+        public DateTime TransactionTime { get; set; } = DateTime.Now;
+
+		/// <summary>
+		/// Статус.
+		/// </summary>
+		public int Status { get; set; }
+
+		/// <summary>
+		/// Итоговая стоймость.
+		/// </summary>
+		public decimal TotalCost { get; set; }
+
+		/// <summary>
+		/// Идентификатор резерва денег.
+		/// </summary>
+		public Guid? BalanceReserveId { get; set; }
+		/// <summary>
+		/// Идентификатор резерва товара.
+		/// </summary>
+		public Guid? ProductReserveId { get; set; }
+
+		// Навигационные свойства.
+		public BalanceReserve BalanceReserve { get; set; }
+		public ProductReserve ProductReserve { get; set; }
+	}
+}
---
+++ Services/PaymentPlatform.Transaction.API/Models/TransactionContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentPlatform.Transaction.API.Models
+{
+    public class ProfileContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+		public DbSet<ProductReserve> ProductReserves { get; set; }
+		public DbSet<BalanceReserve> BalanceReserves { get; set; }
+
+		public ProfileContext(DbContextOptions<ProfileContext> options) : base(options) { }
+    }
+}
