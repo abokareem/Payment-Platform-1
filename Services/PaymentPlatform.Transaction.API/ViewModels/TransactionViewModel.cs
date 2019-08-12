727ae0802891fc48c0e4bdf415b3e7bb22b6b0ca
Горелкин Александр <s207883@yandex.ru>
2019-08-12 18:28:16 +0300

    Заполнена TransactionViewModel

--- Services/PaymentPlatform.Transaction.API/ViewModels/TransactionViewModel.cs
+++ Services/PaymentPlatform.Transaction.API/ViewModels/TransactionViewModel.cs
@@ -7,5 +7,31 @@ namespace PaymentPlatform.Transaction.API.ViewModels
 {
 	public class TransactionViewModel
 	{
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Идентификатор покупателя.
+		/// </summary>
+		public Guid ProfileId { get; set; }
+
+		/// <summary>
+		/// Идентификатор продукта.
+		/// </summary>
+		public Guid ProductId { get; set; }
+
+		/// <summary>
+		/// Время совершения операции.
+		/// </summary>
+		public DateTime TransactionTime { get; set; } = DateTime.Now;
+
+		/// <summary>
+		/// Статус.
+		/// </summary>
+		public int Status { get; set; }
+
+		/// <summary>
+		/// Итоговая стоймость.
+		/// </summary>
+		public decimal TotalCost { get; set; }
 	}
 }
