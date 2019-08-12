16dfcb8e2249058d862d6ddb418d948833c4c6f2
Горелкин Александр <s207883@yandex.ru>
2019-08-12 18:50:33 +0300

    Частичная реализация сервиса транзакций.

--- Services/PaymentPlatform.Transaction.API/Services/Implementations/TransactionService.cs
+++ Services/PaymentPlatform.Transaction.API/Services/Implementations/TransactionService.cs
@@ -24,13 +24,44 @@ namespace PaymentPlatform.Transaction.API.Services.Implementations
 			throw new NotImplementedException();
 		}
 
-		private Task MakeReservesAsync(Models.Transaction transaction)
+		private async Task<bool> MakeReservesAsync(Models.Transaction transaction)
 		{
-			throw new NotImplementedException();
+			var balanceReserve = _mapper.Map<BalanceReserve>(transaction);
+			var productReserve = _mapper.Map<ProductReserve>(transaction);
+
+			//TODO: Decrease balance
+			//TODO: Decrease product
+			await _transactionContext.BalanceReserves.AddAsync(balanceReserve);
+			await _transactionContext.ProductReserves.AddAsync(productReserve);
+			
+			await _transactionContext.SaveChangesAsync();
+
+			if (balanceReserve.Id != null && productReserve.Id != null)
+			{
+				var newTransaction = _mapper.Map<Models.Transaction>(transaction);
+				await _transactionContext.Transactions.AddAsync(newTransaction);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 
 		private Task CancelReserveAsync(Models.Transaction transaction)
 		{
+			var balanceReserve = _transactionContext.BalanceReserves.FirstOrDefault(br => br.Id == transaction.BalanceReserveId);
+			var productReserve = _transactionContext.ProductReserves.FirstOrDefault(br => br.Id == transaction.ProductReserveId);
+
+			if (balanceReserve != null)
+			{
+				//TODO: Increate balance
+			}
+
+			if (productReserve != null)
+			{
+				//TODO: Increate product
+			}
 			throw new NotImplementedException();
 		}
 		public Task<TransactionViewModel> GetTransactionByIdAsync(Guid id)
