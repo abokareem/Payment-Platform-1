93223b4757fedc48833ab3054fcecb7924ba3d60
Михаил Масный <masny.mikhail@gmail.com>
2019-08-23 21:23:46 +0300

    Удалены комментарии.

--- Gateway/PaymentPlatform.Gateway.API/Startup.cs
+++ Gateway/PaymentPlatform.Gateway.API/Startup.cs
@@ -27,8 +27,6 @@ namespace PaymentPlatform.Gateway.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -36,7 +34,6 @@ namespace PaymentPlatform.Gateway.API
             services.AddOcelot(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
