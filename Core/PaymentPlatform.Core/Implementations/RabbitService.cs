2ae9e9db5359add73c10fd6647b23a6671793d8e
Горелкин Александр <s207883@yandex.ru>
2019-08-26 17:37:50 +0300

    Добавлен файл конфигурации. Добавлен класс RabbitService

---
+++ Core/PaymentPlatform.Core/Implementations/RabbitService.cs
@@ -0,0 +1,30 @@
+﻿using PaymentPlatform.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentPlatform.Core.Implementations
+{
+	public class RabbitService : IRabbitService
+	{
+		public System.Threading.Tasks.Task<(bool success, string message)> CheckConnectionAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<(bool success, string message)> ConfigureService(string host, int port, string virtualHost, string userName, string userPassword)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<(bool success, string message)> SendMessageAsync(string message, string recipient)
+		{
+			throw new NotImplementedException();
+		}
+
+		public System.Threading.Tasks.Task<(bool success, string message)> SetListener(Action onIncomingMessage)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Core/PaymentPlatform.Core/Interfaces/IRabbitService.cs
+++ Core/PaymentPlatform.Core/Interfaces/IRabbitService.cs
@@ -8,5 +8,8 @@ namespace PaymentPlatform.Core.Interfaces
 	public interface IRabbitService
 	{
 		Task<(bool success, string message)> SendMessageAsync(string message, string recipient);
+		Task<(bool success, string message)> CheckConnectionAsync();
+		Task<(bool success, string message)> ConfigureService(string host, int port, string virtualHost, string userName, string userPassword);
+		Task<(bool success, string message)> SetListener(Action onIncomingMessage);
 	}
 }
