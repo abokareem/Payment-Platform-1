2e4fa8d7700d815bc293be2fe567cff52873caee
Горелкин Александр <s207883@yandex.ru>
2019-08-26 17:30:40 +0300

    Создана базовая структура Core проекта. Добавлен интерфейс службы брокера сообщений

---
+++ Core/PaymentPlatform.Core/Interfaces/IRabbitService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentPlatform.Core.Interfaces
+{
+	public interface IRabbitService
+	{
+		Task<(bool success, string message)> SendMessageAsync(string message, string recipient);
+	}
+}
