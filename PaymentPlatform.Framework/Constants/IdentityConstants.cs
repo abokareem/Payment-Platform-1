781929de7113c51abfb274c320a4486c48f4d309
Михаил Масный <masny.mikhail@gmail.com>
2019-09-03 20:17:17 +0300

    Добавлены вспомогательные папки в проект *.Framework. Реализованы классы.

---
+++ PaymentPlatform.Framework/Constants/IdentityConstants.cs
@@ -0,0 +1,38 @@
+﻿namespace PaymentPlatform.Framework.Constants
+{
+    /// <summary>
+    /// Константы для проекта Identity.
+    /// </summary>
+    public static class IdentityConstants
+    {
+        /// <summary>
+        /// Роль отсутствует.
+        /// </summary>
+        public static readonly string ROLE_NONE = "None";
+
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public static readonly string ROLE_USER = "User";
+
+        /// <summary>
+        /// Администратор.
+        /// </summary>
+        public static readonly string ROLE_ADMIN = "Admin";
+
+        /// <summary>
+        /// Пользователь существует.
+        /// </summary>
+        public static readonly string USER_EXIST = "Пользователь c таким электронным адресом уже существует.";
+
+        /// <summary>
+        /// Пользователь существует.
+        /// </summary>
+        public static readonly string USER_REGISTRATION_SUCCESS = "Пользователь успешно зарегистрирован.";
+
+        /// <summary>
+        /// Пользователь существует.
+        /// </summary>
+        public static readonly string USER_DATA_INCORRECT = "Логин или пароль неверны.";
+    }
+}
