f6854823f0a60a7ebd2217d45169f3e7a4e168ad
Eugene-Okhryamkin <eugene.okhryamkin@gmail.com>
2019-09-07 18:49:05 +0300

    Вёрстка авторизации

--- Web/WebSPA/src/containers/App.jsx
+++ Web/WebSPA/src/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.sass";
 import Header from "./Header/Header.jsx";
 import Main from "./Main/Main.jsx";
+import Footer from "./Footer/Footer.jsx";
 
 class App extends Component {
     render() {
@@ -9,6 +10,7 @@ class App extends Component {
             <>
                 <Header />
                 <Main />
+                <Footer />
             </>
         )
     }
