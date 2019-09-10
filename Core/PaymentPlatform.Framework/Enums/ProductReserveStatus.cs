af9d5e060d9d9cf32af739ff9048beada13027a8
Горелкин Александр <s207883@yandex.ru>
2019-09-10 12:47:28 +0300

    Добавлено перечисление статусов резерва.

---
+++ Core/PaymentPlatform.Framework/Enums/ProductReserveStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentPlatform.Framework.Enums
+{
+	public enum ProductReserveStatus
+	{
+		NotReserved = 0,
+		Peserved = 1,
+	}
+}
