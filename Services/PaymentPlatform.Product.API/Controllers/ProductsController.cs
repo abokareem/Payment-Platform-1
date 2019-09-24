ccb53ae032bf35673c2f27225ee24bbf25638e3b
Горелкин Александр <s207883@yandex.ru>
2019-09-24 12:20:19 +0300

    Убрана неиспользуемая переменная

--- Services/PaymentPlatform.Product.API/Controllers/ProductsController.cs
+++ Services/PaymentPlatform.Product.API/Controllers/ProductsController.cs
@@ -33,7 +33,7 @@ namespace PaymentPlatform.Product.API.Controllers
 		[HttpGet]
 		public async Task<IEnumerable<ProductViewModel>> GetAllProducts(int? take, int? skip)
 		{
-            var (userId, userRole) = GetClaimsIdentity();
+            var (userId, _) = GetClaimsIdentity();
 
             return await _productService.GetAllProductsAsyc(true, userId, take, skip);
 		}
