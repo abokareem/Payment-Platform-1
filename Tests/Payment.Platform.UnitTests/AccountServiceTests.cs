750a96b669e4d8db43fd856e8e6875dcd2892af6
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-10-29 23:43:43 +0300

    Усовершенствован SeriveFixture. Реализованы тесты для метода RegistrationAsync.

--- Tests/Payment.Platform.UnitTests/AccountServiceTests.cs
+++ Tests/Payment.Platform.UnitTests/AccountServiceTests.cs
@@ -1,7 +1,115 @@
-﻿namespace Payment.Platform.UnitTests
-{
-    public class AccountServiceTests
-    {
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PaymentPlatform.Framework.Constants;
+using PaymentPlatform.Framework.Helpers;
+using PaymentPlatform.Framework.ViewModels;
+using PaymentPlatform.Identity.API.Models;
+using PaymentPlatform.Identity.API.Services.Implementations;
+using PaymentPlatform.Identity.API.Services.Interfaces;
+using System;
+using Xunit;
 
+namespace Payment.Platform.UnitTests
+{
+    /// <summary>
+    /// Класс для тестов класса AccountService.
+    /// </summary>
+    public class AccountServiceTests : IClassFixture<ServiceFixture>
+    {
+        private readonly ServiceProvider _serviceProvider;
+        private readonly IMapper _mapper;
+        private readonly IOptions<AppSettings> _options;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="fixture">приспособление для внедрения DI и InMemoryDatabase.</param>
+        public AccountServiceTests(ServiceFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+            _options = _serviceProvider.GetRequiredService<IOptions<AppSettings>>();
+            _mapper = _serviceProvider.GetRequiredService<IMapper>();
+        }
+
+        /// <summary>
+        /// Формирование настроек для MainContext в InMemoryDatabase.
+        /// </summary>
+        /// <returns>Настройки MainContext.</returns>
+        private DbContextOptions<IdentityContext> GetContextOptions()
+        {
+            var options = new DbContextOptionsBuilder<IdentityContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return options;
+        }
+
+        /// <summary>
+        /// Тест на регистрацию пользователя.
+        /// </summary>
+        [Fact]
+        public void CanUserBeRegistered_Return_True()
+        {
+            // Arrange
+            var options = GetContextOptions();
+            var accountViewModel = new AccountViewModel
+            {
+                Login = "Login",
+                Email = "Email@Email.Email",
+                Password = "P@ssword",
+                Role = 1,
+                IsActive = true
+            };
+
+            var result = false;
+            var message = string.Empty;
+
+            // Act
+            using (var context = new IdentityContext(options))
+            {
+                IAccountService accountService = new AccountService(_options, context, _mapper);
+                (result, message) = accountService.RegistrationAsync(accountViewModel).GetAwaiter().GetResult();
+            }
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(IdentityConstants.USER_REGISTRATION_SUCCESS, message);
+        }
+
+        /// <summary>
+        /// Тест на регистрацию пользователя, если пользователь c таким электронным адресом уже существует.
+        /// </summary>
+        [Fact]
+        public void CanUserBeRegistered_Return_False()
+        {
+            // Arrange
+            var options = GetContextOptions();
+            var accountViewModel = new AccountViewModel
+            {
+                Login = "Login",
+                Email = "Email@Email.Email",
+                Password = "P@ssword",
+                Role = 1,
+                IsActive = true
+            };
+
+            var result = false;
+            var message = string.Empty;
+
+            // Act
+            using (var context = new IdentityContext(options))
+            {
+                IAccountService accountService = new AccountService(_options, context, _mapper);
+                _ = accountService.RegistrationAsync(accountViewModel).GetAwaiter().GetResult();
+
+                (result, message) = accountService.RegistrationAsync(accountViewModel).GetAwaiter().GetResult();
+            }
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(IdentityConstants.USER_EXIST, message);
+        }
     }
 }
--- Tests/Payment.Platform.UnitTests/ServiceFixture.cs
+++ Tests/Payment.Platform.UnitTests/ServiceFixture.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using PaymentPlatform.Identity.API.Services.Implementations;
-using PaymentPlatform.Identity.API.Services.Interfaces;
-using PaymentPlatform.Product.API.Services.Implementations;
-using PaymentPlatform.Product.API.Services.Interfaces;
-using PaymentPlatform.Profile.API.Services.Implementations;
-using PaymentPlatform.Profile.API.Services.Interfaces;
-using PaymentPlatform.Transaction.API.Services.Implementations;
-using PaymentPlatform.Transaction.API.Services.Interfaces;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PaymentPlatform.Framework.Helpers;
+using PaymentPlatform.Framework.Mapping;
 
 namespace Payment.Platform.UnitTests
 {
@@ -27,11 +23,29 @@ namespace Payment.Platform.UnitTests
         {
             var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddScoped<IAccountService, AccountService>();
-            serviceCollection.AddScoped<IProductService, ProductService>();
-            serviceCollection.AddScoped<IProfileService, ProfileService>();
-            serviceCollection.AddScoped<ITransactionService, TransactionService>();
+            // Конфигурация AutoMapper
+            var autoMapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AccountProfile());
+                mc.AddProfile(new ProductProfile());
+                mc.AddProfile(new UserProfile());
+                mc.AddProfile(new TransactionProfile());
+            });
+            var mapper = autoMapperConfig.CreateMapper();
 
+            // Конфигурация IOptions<AppSettings>
+            var appSettings = new AppSettings() { Secret = "SecretKey" };
+            var options = Options.Create(appSettings);
+
+            serviceCollection.AddSingleton(options);
+            serviceCollection.AddSingleton(mapper);
+
+            // UNDONE: Удалить по итогу (?)
+            //serviceCollection.AddDbContext<IdentityContext>(context => context.UseInMemoryDatabase(Guid.NewGuid().ToString()), ServiceLifetime.Transient);
+            //serviceCollection.AddScoped<IAccountService, AccountService>();
+            //serviceCollection.AddScoped<IProductService, ProductService>();
+            //serviceCollection.AddScoped<IProfileService, ProfileService>();
+            //serviceCollection.AddScoped<ITransactionService, TransactionService>();
             //serviceCollection.AddScoped<IRandomDataGeneratorService, RandomDataGeneratorService>();
             //serviceCollection.AddSingleton<IRabbitMQService, RabbitMQService>();
 
