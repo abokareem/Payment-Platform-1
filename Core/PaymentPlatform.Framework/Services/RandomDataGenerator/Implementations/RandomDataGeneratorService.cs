8d28dc7fa0e9072abaa3e25371f3d6b95b098f91
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2019-11-01 22:49:23 +0300

    Рефакторинг AddNewTransactionsAsync. Реализованы тесты для метода AddNewTransactionsAsync.

--- Core/PaymentPlatform.Framework/Services/RandomDataGenerator/Implementations/RandomDataGeneratorService.cs
+++ Core/PaymentPlatform.Framework/Services/RandomDataGenerator/Implementations/RandomDataGeneratorService.cs
@@ -71,7 +71,9 @@ namespace PaymentPlatform.Framework.Services.RandomDataGenerator.Implementations
             var rnd = new Random();
 
             var products = new List<ProductContextModel>();
-            var profilesId = _mainContext.Profiles.Where(p => p.IsSeller).Select(p => p.Id).ToList();
+            var profilesId = _mainContext.Profiles.Where(p => p.IsSeller)
+                                                  .Select(p => p.Id)
+                                                  .ToList();
 
             if (!profilesId.Any())
             {
@@ -124,12 +126,59 @@ namespace PaymentPlatform.Framework.Services.RandomDataGenerator.Implementations
             var transactions = new List<TransactionContextModel>();
 
             var profilesId = _mainContext.Profiles.Where(p => p.IsSeller == true)
-                                        .Select(p => p.Id)
-                                        .ToList();
+                                                  .Select(p => p.Id)
+                                                  .ToList();
+
+            if (!profilesId.Any())
+            {
+                var profile = new ProfileContextModel
+                {
+                    FirstName = Guid.NewGuid().ToString().Substring(0, 8),
+                    LastName = Guid.NewGuid().ToString().Substring(0, 8),
+                    SecondName = Guid.NewGuid().ToString().Substring(0, 8),
+                    IsSeller = true,
+                    OrgName = Guid.NewGuid().ToString().Substring(0, 8),
+                    OrgNumber = Guid.NewGuid().ToString().Substring(0, 8),
+                    BankBook = Guid.NewGuid().ToString().ToUpper(),
+                    Balance = new Random(10).Next(10000)
+                };
+
+                await _mainContext.Profiles.AddAsync(profile);
+                await _mainContext.SaveChangesAsync();
+
+                var profileId = _mainContext.Profiles.FirstOrDefault().Id;
+                profilesId.Add(profileId);
+            }
 
             var productsId = _mainContext.Products.Where(p => p.IsActive == true)
-                                        .Select(p => p.Id)
-                                        .ToList();
+                                                  .Select(p => p.Id)
+                                                  .ToList();
+
+            if (!productsId.Any())
+            {
+                var profileId = _mainContext.Profiles.Where(p => p.IsSeller == true)
+                                                     .Select(p => p.Id)
+                                                     .FirstOrDefault();
+
+                var product = new ProductContextModel
+                {
+                    ProfileId = profileId,
+                    Name = Guid.NewGuid().ToString().Substring(0, 8),
+                    Description = Guid.NewGuid().ToString().ToUpper(),
+                    MeasureUnit = Guid.NewGuid().ToString().ToUpper().Substring(0, 4),
+                    Category = Guid.NewGuid().ToString().Substring(0, 8),
+                    Amount = new Random(0).Next(10000),
+                    Price = new Random(10).Next(10000),
+                    QrCode = Guid.NewGuid().ToString().ToUpper(),
+                    IsActive = true
+                };
+
+                await _mainContext.Products.AddAsync(product);
+                await _mainContext.SaveChangesAsync();
+
+                var productId = _mainContext.Products.FirstOrDefault().Id;
+                productsId.Add(productId);
+            }
 
             for (int i = 0; i < count; i++)
             {
--- Tests/Payment.Platform.UnitTests/RandomDataGeneratorServiceTests.cs
+++ Tests/Payment.Platform.UnitTests/RandomDataGeneratorServiceTests.cs
@@ -119,5 +119,88 @@ namespace Payment.Platform.UnitTests
             // Assert
             Assert.Equal(countOfEntities, countOfProducts);
         }
+
+        /// <summary>
+        /// Тест на корректное заполнение базы данных случайными данными.
+        /// </summary>
+        [Fact]
+        public void AddNewTransactions_Return_1_Entity()
+        {
+            // Arrange
+            var options = GetContextOptions();
+
+            var profile = new ProfileContextModel
+            {
+                FirstName = Guid.NewGuid().ToString().Substring(0, 8),
+                LastName = Guid.NewGuid().ToString().Substring(0, 8),
+                SecondName = Guid.NewGuid().ToString().Substring(0, 8),
+                IsSeller = true,
+                OrgName = Guid.NewGuid().ToString().Substring(0, 8),
+                OrgNumber = Guid.NewGuid().ToString().Substring(0, 8),
+                BankBook = Guid.NewGuid().ToString().ToUpper(),
+                Balance = new Random(10).Next(10000)
+            };
+
+            var countOfEntities = 1;
+            var countOfTransactions = 0;
+
+            // Act
+            using (var context = new MainContext(options))
+            {
+                context.Profiles.Add(profile);
+                context.SaveChanges();
+
+                var profileId = context.Profiles.LastOrDefault().Id;
+
+                var product = new ProductContextModel
+                {
+                    ProfileId = profileId,
+                    Name = Guid.NewGuid().ToString().Substring(0, 8),
+                    Description = Guid.NewGuid().ToString().ToUpper(),
+                    MeasureUnit = Guid.NewGuid().ToString().ToUpper().Substring(0, 4),
+                    Category = Guid.NewGuid().ToString().Substring(0, 8),
+                    Amount = new Random(0).Next(10000),
+                    Price = new Random(10).Next(10000),
+                    QrCode = Guid.NewGuid().ToString().ToUpper(),
+                    IsActive = true
+                };
+
+                context.Products.Add(product);
+                context.SaveChanges();
+
+                IRandomDataGeneratorService randomDataGeneratorService = new RandomDataGeneratorService(context);
+                randomDataGeneratorService.AddNewTransactionsAsync(countOfEntities).GetAwaiter().GetResult();
+
+                countOfTransactions = context.Products.ToList().Count;
+            }
+
+            // Assert
+            Assert.Equal(countOfEntities, countOfTransactions);
+        }
+
+        /// <summary>
+        /// Тест на корректное заполнение базы данных случайными данными.
+        /// </summary>
+        [Fact]
+        public void AddNewTransactions_WhenNoOneProfileIsSeller_And_WhenNoOneProducIsActive_Return_1_Entity()
+        {
+            // Arrange
+            var options = GetContextOptions();
+
+            var countOfEntities = 1;
+            var countOfTransactions = 0;
+
+            // Act
+            using (var context = new MainContext(options))
+            {
+                IRandomDataGeneratorService randomDataGeneratorService = new RandomDataGeneratorService(context);
+                randomDataGeneratorService.AddNewTransactionsAsync(countOfEntities).GetAwaiter().GetResult();
+
+                countOfTransactions = context.Products.ToList().Count;
+            }
+
+            // Assert
+            Assert.Equal(countOfEntities, countOfTransactions);
+        }
     }
 }
